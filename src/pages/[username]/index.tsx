76165323b6d1487b1d49e60b4ec12be136f1e0d5
balogunofafrica <balogunakanbi.k@gmail.com>
2023-04-10 15:08:46 +0100

    refactor: profile page

--- src/pages/[username]/index.tsx
+++ src/pages/[username]/index.tsx
@@ -1,27 +1,15 @@
 import CurrentJobsTable from "@/components/tables/CurrentJobsTable";
 import PastJobsTable from "@/components/tables/PastJobsTable";
-import useTranscripts from "@/hooks/useTranscripts";
 import { Heading } from "@chakra-ui/react";
 
-export default function Home() {
-  const { data, isLoading, isRefetching, refetch } =
-    useTranscripts().transcripts;
-
+export default function Profile() {
   return (
     <>
       <Heading size="md" mb={6}>
         My Account
       </Heading>
-      <CurrentJobsTable
-        data={data ?? []}
-        isLoading={isLoading || isRefetching}
-        refetch={refetch}
-      />
-      <PastJobsTable
-        data={data ?? []}
-        isLoading={isLoading || isRefetching}
-        refetch={refetch}
-      />
+      <CurrentJobsTable />
+      <PastJobsTable />
     </>
   );
 }
