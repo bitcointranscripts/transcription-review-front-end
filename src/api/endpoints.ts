2ed86a2e81b650fc25ef965249fc33e302fb27fb
balogunofafrica <balogunakanbi.k@gmail.com>
2023-04-25 14:08:31 +0100

    chore: add endpoint for archiving transcripts

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,3 +1,5 @@
+const ARCHIVE_TRANSCRIPTS_BY_ID = (id: number) => `transcripts/${id}/archive`;
+
 const GET_TRANSCRIPTS = () => `transcripts`;
 
 const GET_TRANSCRIPTS_BY_ID = (id: number) => `transcripts/${id}`;
@@ -7,6 +9,7 @@ const USERS = () => `users`;
 const REVIEWS = (id?: number) => (id ? `reviews/${id}` : `reviews`);
 
 const endpoints = {
+  ARCHIVE_TRANSCRIPTS_BY_ID,
   GET_TRANSCRIPTS,
   GET_TRANSCRIPTS_BY_ID,
   USERS,
