c480a13bea0b125ec0d750d7fec79a269af8b28c
Emmanuel-Develops <itakpeemma@gmail.com>
2023-05-05 19:26:51 +0100

    refactor: new claim endpoint

--- src/services/api/endpoints.ts
+++ src/services/api/endpoints.ts
@@ -1,5 +1,7 @@
 const ARCHIVE_TRANSCRIPTS_BY_ID = (id: number) => `transcripts/${id}/archive`;
 
+const CLAIM_TRANSCRIPT = (id: number) => `transcripts/${id}/claim`;
+
 const GET_TRANSCRIPTS = () => `transcripts`;
 
 const GET_TRANSCRIPTS_BY_ID = (id: number) => `transcripts/${id}`;
@@ -12,6 +14,7 @@ const REVIEWS = (id?: number) => (id ? `reviews/${id}` : `reviews`);
 
 const endpoints = {
   ARCHIVE_TRANSCRIPTS_BY_ID,
+  CLAIM_TRANSCRIPT,
   GET_TRANSCRIPTS,
   GET_TRANSCRIPTS_BY_ID,
   USERS,
