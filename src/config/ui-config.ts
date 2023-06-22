0f1badfcfe43f1547072ea4cdc7cfee232460532
Emmanuel-Develops <itakpeemma@gmail.com>
2023-06-22 12:56:36 +0100

    fix: remove container restriction on tutorial route

--- src/config/ui-config.ts
+++ src/config/ui-config.ts
@@ -1,6 +1,13 @@
-// eslint-disable-next-line import/no-anonymous-default-export
+/* eslint-disable import/no-anonymous-default-export */
+
+// FULL_WIDTH_ROUTES are routes with no width restriction on their containers
+
+const ROUTES_CONFIG = {
+  HOME: "",
+  TUTORIAL: "tutorial",
+};
 export default {
-  FULL_WIDTH_ROUTES: [""],
+  FULL_WIDTH_ROUTES: [ROUTES_CONFIG["HOME"], ROUTES_CONFIG["TUTORIAL"]],
   YOUTUBE_TIMESTAMP_IN_SECONDS: {
     1: 136,
     2: 152,
