3a87b5b7f6ba16d4b00ddf19f8b5e8f3913dffc1
Emmanuel-Develops <itakpeemma@gmail.com>
2023-06-29 23:09:13 +0100

    fix: prop rename and remove unsed inports

--- src/components/home/Queuer.tsx
+++ src/components/home/Queuer.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 import QueueTable from "@/components/tables/QueueTable";
-import { Box, Heading, Link, Text } from "@chakra-ui/react";
 import GlobalContainer from "../GlobalContainer";
 
 export default function HomePage() {
