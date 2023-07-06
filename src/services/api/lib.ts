d17aec62523ffe15af84cb9e2e1e88e8d9a9d275
Theophilus <isahtheophilus@gmail.com>
2023-07-06 22:15:28 +0100

    chore: update acct creation logic

--- src/services/api/lib.ts
+++ src/services/api/lib.ts
@@ -1,19 +1,22 @@
 import axios from "./axios";
 import endpoints from "./endpoints";
 
-type CreateUserProp = {
+export type CreateUserProp = {
   username: string;
   permissions?: string;
+  email: string;
 };
 
 type UserData = {
   data: {
     permissions: "reviewer" | "admin";
     id: number;
+    email: string;
     githubUsername: string;
     updatedAt: string;
     createdAt: string;
     authToken?: string | null;
+    jwt?: string | null;
     archivedAt?: string | null;
   };
 };
@@ -21,9 +24,26 @@ type UserData = {
 export const createNewUser = async ({
   username,
   permissions,
+  email,
 }: CreateUserProp): Promise<UserData> => {
   return axios.post(endpoints.USERS(), {
     username,
     permissions,
+    email,
+  });
+};
+
+// TODO: account for other properties e.g. permissions, jwt, etc.
+export const updateUserProfile = async ({
+  id,
+  email,
+  username,
+}: CreateUserProp & { id: number }): Promise<UserData> => {
+  if (!id) {
+    throw new Error("User id is required");
+  }
+  return axios.put(endpoints.USER_BY_ID(id), {
+    email,
+    username,
   });
 };
