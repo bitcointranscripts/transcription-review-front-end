e7151b0e7e65812d28ddc564e05ce390ba150fc3
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-07-07 13:39:02 +0100

    fix: changed html to text

--- src/components/editTranscript/EditTranscript.tsx
+++ src/components/editTranscript/EditTranscript.tsx
@@ -49,8 +49,8 @@ const EditTranscript = ({
   const mdParser = new MarkdownIt(/* Markdown-it options */);
 
   // Finish!
-  function handleEditorChange({ html }: IHandleEditorChange) {
-    update(html);
+  function handleEditorChange({ text }: IHandleEditorChange) {
+    update(text);
   }
   // hijack params of mdEditor to change toolbar "preview" function
   useEffect(() => {
