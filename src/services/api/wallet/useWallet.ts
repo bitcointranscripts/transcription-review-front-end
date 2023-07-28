cc7e7e6c535d3b373367f6ec3149071925a69b5d
Theophilus <isahtheophilus@gmail.com>
2023-07-28 10:07:30 +0100

    chore: remove promise.all

--- src/services/api/wallet/useWallet.ts
+++ src/services/api/wallet/useWallet.ts
@@ -6,18 +6,11 @@ import endpoints from "../endpoints";
 import { Wallet } from "../../../../types";
 
 const getWallet = async (userId?: number): Promise<Wallet> => {
-  const txResponse = axios.get(endpoints.GET_TRANSACTIONS({ userId }));
-  const walletResponse = axios.get(endpoints.GET_WALLET(userId));
-  if (txResponse && walletResponse) {
-    return Promise.all([txResponse, walletResponse])
-      .then((res) => {
-        const transactions = res[0].data;
-        const wallet = res[1].data;
-        return { ...wallet, transactions };
-      })
-      .catch((err) => err);
+  const walletResponse = await axios.get(endpoints.GET_WALLET(userId));
+  if (walletResponse.status !== 200) {
+    throw new Error("Failed to fetch wallet");
   }
-  return Promise.reject("Error");
+  return walletResponse.data as unknown as Wallet;
 };
 
 export const useGetWallet = (id?: number) =>
