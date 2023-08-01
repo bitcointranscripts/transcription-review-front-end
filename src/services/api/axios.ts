3b30c216039033c08e7ac4f8ae5e43ce2e572bc0
Emmanuel-Develops <itakpeemma@gmail.com>
2023-08-01 17:57:21 +0100

    fix: signOut and sign back In on invalid token

--- src/services/api/axios.ts
+++ src/services/api/axios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { signIn, signOut } from "next-auth/react";
 import { getSession } from "next-auth/react";
 
 const axiosInstance = axios.create({
@@ -14,5 +14,23 @@ axiosInstance.interceptors.request.use(async (config) => {
 
   return config;
 });
+
+axiosInstance.interceptors.response.use(
+  async (response) => {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  async (error) => {
+    const statusCode = error.response.status;
+    if (statusCode === 401) {
+      await signOut({ redirect: false });
+      await signIn("github");
+    }
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
