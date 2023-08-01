64c4ca6952b1699b22f3b1615cf448e5db354410
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-08-01 22:25:32 +0100

    fix: added categories as a label, removed the none tag from transcripts

--- src/components/tables/TitleWithTags.tsx
+++ src/components/tables/TitleWithTags.tsx
@@ -1,19 +1,43 @@
-import { tagColors } from "@/utils";
+import { tagColors, transcriptsCategories } from "@/utils";
 import { Box, Flex, Td, Text } from "@chakra-ui/react";
 import React from "react";
 
 type TitleWithTagsProps = {
   title: string;
   id: number;
+  categories: string | string[];
   allTags: string[];
   length: number;
 };
-const TitleWithTags = ({ title, allTags, id, length }: TitleWithTagsProps) => {
+const TitleWithTags = ({
+  title,
+  allTags,
+  categories,
+  id,
+  length,
+}: TitleWithTagsProps) => {
+  const stringCategories = Array.isArray(categories)
+    ? categories[0]
+    : categories;
+  const foundCategories = transcriptsCategories.find(
+    (trs) => trs.slug.toLowerCase() === stringCategories.toLocaleLowerCase()
+  );
   return (
     <Td width="40%">
       <Flex gap={2} alignItems="center">
         <Text>{title}</Text>
         <Flex wrap="wrap" gap={2} alignItems="center">
+          {categories && (
+            <Box
+              borderRadius={"4px"}
+              padding={"6px"}
+              bgColor={tagColors[categories.length % 4]}
+            >
+              <Text textTransform="capitalize" color="#FCFCFC">
+                {foundCategories?.name}
+              </Text>
+            </Box>
+          )}
           {allTags.slice(0, 2).map((tags, index) => (
             <Box
               borderRadius={"4px"}
