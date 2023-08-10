f441f3b94ecd569b2d80889b833c4ad077bc11b8
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-08-10 19:09:22 +0100

    fix: added persiting of pageNum and using useQueries to make a promise.all call

--- src/components/tables/Pagination.tsx
+++ src/components/tables/Pagination.tsx
@@ -1,5 +1,6 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
-import React, { Dispatch, SetStateAction } from "react";
+import { useRouter } from "next/router";
+import React, { Dispatch, SetStateAction, useLayoutEffect } from "react";
 
 type IPagination = {
   pages: number;
@@ -7,13 +8,25 @@ type IPagination = {
   setCurrentPage: Dispatch<SetStateAction<number>>;
 };
 const Pagination = ({ pages, currentPage, setCurrentPage }: IPagination) => {
+  const router = useRouter();
+  const handleNextPage = (page: number) => {
+    router.replace({
+      query: { ...router.query, pageNum: page },
+    });
+    setCurrentPage(page);
+  };
+  useLayoutEffect(() => {
+    if (router.query?.pageNum) {
+      setCurrentPage(Number(router.query?.pageNum as string));
+    }
+  }, [router.query.pageNum, setCurrentPage]);
   return (
     <Flex justifyContent={"center"}>
       {pages > 1 &&
         Array.from({ length: pages }, (_, index) => index + 1).map((item) => (
           <Button
             colorScheme={currentPage === item ? "orange" : "gray"}
-            onClick={() => setCurrentPage(item)}
+            onClick={() => handleNextPage(item)}
             key={item}
             variant="ghost"
           >
