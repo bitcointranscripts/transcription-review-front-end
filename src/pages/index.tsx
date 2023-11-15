aab37e56c7b078032af8227ba0f4099690f64689
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-11-15 15:50:36 +0100

    fix: created /home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,38 @@
-/* eslint-disable react/no-unescaped-entities */
-import { NextPage } from "next";
+import { GetServerSideProps, GetServerSidePropsContext, NextPage } from "next";
 import type { Session } from "next-auth";
+import { getServerSession } from "next-auth/next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { authOptions } from "./api/auth/[...nextauth]";
 import LandingPage from "@/components/home/LandingPage";
 
-export type HomePageProps = {
+type HomePageProps = {
   serverSession: Session | null;
 };
 
-const Home: NextPage<HomePageProps> = () => {
+const Home: NextPage<HomePageProps> = ({ serverSession }) => {
+  const router = useRouter();
+  useEffect(() => {
+    if (serverSession) {
+      router.push(`/${serverSession.user?.githubUsername || "no-user"}`);
+    }
+  }, [serverSession, router]);
+  if (serverSession) {
+    return <></>;
+  }
+
   return <LandingPage />;
+};
+
+export const getServerSideProps: GetServerSideProps<HomePageProps> = async (
+  context: GetServerSidePropsContext
+) => {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  return {
+    props: {
+      serverSession: session,
+    },
+  };
 };
 
 export default Home;
