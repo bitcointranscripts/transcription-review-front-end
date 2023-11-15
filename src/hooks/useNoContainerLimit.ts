4635c0e5f6d8829c01c54d26a745fc472b113838
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-11-15 21:10:46 +0100

    fix: added padding with navbar for only /home route

--- src/hooks/useNoContainerLimit.ts
+++ src/hooks/useNoContainerLimit.ts
@@ -5,8 +5,8 @@ const useNoContainerLimit = () => {
   const router = useRouter();
   const path = router.asPath?.split("/")[1] ?? "";
   const noRestriction = UI_CONFIG.FULL_WIDTH_ROUTES.includes(path);
-
-  return { noRestriction };
+  const homeRestriction = UI_CONFIG.FULL_NAV_ROUTES.includes(path);
+  return { noRestriction, homeRestriction };
 };
 
 export default useNoContainerLimit;
