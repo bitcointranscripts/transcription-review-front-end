ee3e036d1e44ef3550c1128305b54889ac85ed0f
Emmanuel-Develops <itakpeemma@gmail.com>
2023-11-15 18:24:15 +0100

    feat: launch checklist, prevent signIn on mobile

--- src/components/home/LandingPage.tsx
+++ src/components/home/LandingPage.tsx
@@ -6,16 +6,28 @@ import ReviewingProcess from "./ReviewingProcess";
 import EverythingYouNeed from "./EverythingYouNeed";
 import ReadyToReview from "./ReadyToReview";
 import LandingFooter from "./LandingFooter";
+import useMobileWarning from "@/hooks/useMobileWarning";
+import MobileWarningModal from "../mobileUnsupported/MobileWarningModal";
+import { signIn } from "next-auth/react";
 
 const LandingPage = () => {
+  const { isMobileAction, showWarning, closeWarning } = useMobileWarning();
+
+  const getStarted = async () => {
+    const mobileAction = isMobileAction();
+    if (!mobileAction) {
+      signIn("github", { callbackUrl: "/signin", redirect: true });
+    }
+  };
   return (
     <Box>
-      <HeroSection />
+      <HeroSection getStarted={getStarted} />
       <NeedSection />
       <EverythingYouNeed />
       <ReviewingProcess />
-      <ReadyToReview />
+      <ReadyToReview getStarted={getStarted} />
       <LandingFooter />
+      <MobileWarningModal isOpen={showWarning} onClose={closeWarning} />
     </Box>
   );
 };
