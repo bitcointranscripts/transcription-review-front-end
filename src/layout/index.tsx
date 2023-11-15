24d71ac111fa2d2007a55bf630c351c6d474d22a
Adam Jonas <jonas@chaincode.com>
2023-11-14 21:10:40 -0500

    Twitter card and meta data

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -12,25 +12,33 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const isHomePage = router.asPath === "/";
   return (
-    <div className={styles.app_container}>
-      <Head>
-        <title>Bitcoin Transcription Reviews</title>
-        <meta name="description" content="Bitcoin Transcription Review" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="./btc-transcript-circle-128.png" />
-      </Head>
-      {!isHomePage && <Navbar />}
-      <GlobalContainer
-        flexGrow={1}
-        py={!isHomePage ? 4 : 0}
-        mt={!isHomePage ? 12 : 0}
-        {...(noRestriction ? { maxW: "none", p: 0 } : {})}
-      >
-        {children}
-      </GlobalContainer>
-      {!isHomePage && <Footer />}
-    </div>
-    <script async src="https://visits.bitcoindevs.xyz/script.js" data-website-id="d9b96a7b-a2db-4ef1-9360-69d3b288859d"></script>
+    <>
+      <div className={styles.app_container}>
+        <Head>
+          <title>Bitcoin Transcripts Reviews</title>
+          <meta name="description" content="Review Technical Bitcoin Transcripts and Earn Sats" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="./btc-transcript-circle-128.png" />
+          <meta property="og:image" content="https://review.btctranscripts.com/btctranscripts.png"></meta>
+          <meta property="og:title" content="Bitcoin Transcripts Reviews"></meta>
+          <meta property="og:url" content="https://review.btctranscripts.com"></meta>
+          <meta property="og:description" content="Review Technical Bitcoin Transcripts and Earn Sats"></meta>
+          <meta name="twitter:card" content="summary"></meta>
+          <meta name="twitter:image" content="https://review.btctranscripts.com/btctranscripts.png"></meta>
+        </Head>
+        {!isHomePage && <Navbar />}
+        <GlobalContainer
+          flexGrow={1}
+          py={!isHomePage ? 4 : 0}
+          mt={!isHomePage ? 12 : 0}
+          {...(noRestriction ? { maxW: "none", p: 0 } : {})}
+        >
+          {children}
+        </GlobalContainer>
+        {!isHomePage && <Footer />}
+      </div>
+      <script async src="https://visits.bitcoindevs.xyz/script.js" data-website-id="d9b96a7b-a2db-4ef1-9360-69d3b288859d"></script>
+    </>
   );
 };
 
