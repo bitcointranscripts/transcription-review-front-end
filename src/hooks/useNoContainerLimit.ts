c01222274cc796436e9a6e03e5c8edcda0f1ee73
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-11-17 23:07:14 +0100

    fix: changed all router.aspath to router.pathname

--- src/hooks/useNoContainerLimit.ts
+++ src/hooks/useNoContainerLimit.ts
@@ -3,7 +3,7 @@ import { UI_CONFIG } from "@/config/ui-config";
 
 const useNoContainerLimit = () => {
   const router = useRouter();
-  const path = router.asPath?.split("/")[1] ?? "";
+  const path = router.pathname?.split("/")[1] ?? "";
   const noRestriction = UI_CONFIG.FULL_WIDTH_ROUTES.includes(path);
   const homeRestriction = UI_CONFIG.FULL_NAV_ROUTES.includes(path);
   return { noRestriction, homeRestriction };
