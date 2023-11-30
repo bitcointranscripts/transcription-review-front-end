6be3deb3592a27adc71f4be3f10de84c6e6d5146
0tuedon <90271995+0tuedon@users.noreply.github.com>
2023-11-30 19:39:15 +0100

    chore:  reduced font sizing in hero section and need section

--- src/components/home/ThingsNeededSingle.tsx
+++ src/components/home/ThingsNeededSingle.tsx
@@ -89,14 +89,13 @@ const ThingsNeededSingle: FC<IThingsNeeded> = ({
           fontWeight={400}
           textAlign={{ base: "center", lg: "left" }}
           whiteSpace={"pre-line"}
-          fontSize={[
-            "0.875rem",
-            "0.875rem",
-            "1.05rem",
-            "1.05rem",
-            "1.05rem",
-            "1.5rem",
-          ]}
+          fontSize={{
+            base: "0.875rem",
+            md: "1.05rem",
+            xl: "1.05rem",
+            "2xl": "1.3rem",
+            "3xl": "1.5rem",
+          }}
         >
           {sub}{" "}
           {linkText && (
