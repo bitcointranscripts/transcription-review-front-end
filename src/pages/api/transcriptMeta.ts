7894ccdb2b68065427c139fece59e63d4e507b37
kouloumos <kouloumosa@gmail.com>
2024-01-26 16:35:18 +0200

    Use `btctranscripts.com/status.json` for `api/transcriptMeta`

--- src/pages/api/transcriptMeta.ts
+++ src/pages/api/transcriptMeta.ts
@@ -1,45 +1,34 @@
 import axios from "axios";
 import type { NextApiRequest, NextApiResponse } from "next";
-import * as cheerio from "cheerio";
+
 import config from "@/config/config.json";
 import { SelectableMetaDataType } from "../../../types";
+
+// Generic function to transform an array of strings to SelectableMetaDataType
+function transformToSelectableMetaData(arr: string[]): SelectableMetaDataType[] {
+  return arr.map(item => ({
+    slug: item,
+    value: item,
+  }));
+}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const getMetaFromResults = (data: any) => {
-    let _array: SelectableMetaDataType[] = [];
-    const $ = cheerio.load(data);
-    const ulResults = $(".container-fluid .main ul");
-    const categoriesLink = ulResults.find("li a");
-    categoriesLink.each((_index, element) => {
-      const _possibleslug = $(element).attr("href")?.split("/")[2];
-      const value = $(element).text();
-      const slug = _possibleslug ?? value.replaceAll(" ", "").toLowerCase();
-      _array.push({ slug, value });
-    });
-
-    return _array.sort((a, b) =>
-      a.value.toLowerCase().localeCompare(b.value.toLowerCase())
-    );
-  };
-
-  const makeRequest = async (url: string) => {
-    try {
-      const response = await axios.get(url);
-      const finalArray = getMetaFromResults(response.data);
-      return finalArray;
-    } catch (error) {
-      throw error; // Re-throw the error to reject the Promise
-    }
-  };
   try {
-    const [categories, speakers, tags] = await Promise.all([
-      makeRequest(config.btctranscripts_categories_url),
-      makeRequest(config.btctranscripts_speakers_url),
-      makeRequest(config.btctranscripts_tags_url),
-    ]);
+    const response = await axios.get(`${config.btctranscripts_base_url}status.json`);
+
+    if (response.status !== 200) {
+      throw Error(`HTTP error! Status: ${response.status}`)
+    }
+    const data = response.data.existing;
+
+    // Use the generic function to transform arrays
+    const categories = transformToSelectableMetaData(data.categories);
+    const speakers = transformToSelectableMetaData(data.speakers);
+    const tags = transformToSelectableMetaData(data.tags);
+
     res.status(200).json({ categories, speakers, tags });
   } catch (error) {
     console.error(error);
