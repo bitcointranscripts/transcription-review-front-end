b9c35f8fd69776737f19cbd19bd5a96e7ec62942
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-02-07 18:17:26 +0100

    style: moved save and submit to the top of the md editor
    fix: made sidebar content a self closing tag

--- src/components/editTranscript/EditTranscript.tsx
+++ src/components/editTranscript/EditTranscript.tsx
@@ -10,13 +10,27 @@ import {
   ModalHeader,
   ModalOverlay,
   Text,
+  Tooltip,
+  useToast,
 } from "@chakra-ui/react";
 
 import MdEditor from "react-markdown-editor-lite";
 import "react-markdown-editor-lite/lib/index.css";
-import { MutableRefObject, useEffect, useState } from "react";
+import {
+  Dispatch,
+  MutableRefObject,
+  SetStateAction,
+  useEffect,
+  useState,
+} from "react";
 import MarkdownIt from "markdown-it";
 import { FaBook } from "react-icons/fa";
+import SubmitTranscriptMenu, {
+  TranscriptSubmitOptions,
+} from "../menus/SubmitTranscriptMenu";
+import { TranscriptContent, UserReviewData } from "../../../types";
+import { useSession } from "next-auth/react";
+import { useUpdateTranscript } from "@/services/api/transcripts";
 
 // Interfaces for react-markdown-editior
 export interface IHandleEditorChange {
@@ -35,18 +49,69 @@ const EditTranscript = ({
   restoreOriginal,
   editorRef,
   openGuidelines,
+  reviewData,
+  saveTranscript,
+  getUpdatedTranscript,
+  onOpen,
+  prRepo,
+  setPrRepo,
 }: {
   mdData: string;
+  prRepo: TranscriptSubmitOptions;
+  setPrRepo: Dispatch<SetStateAction<TranscriptSubmitOptions>>;
   editorRef: MutableRefObject<MdEditor | null>;
   // eslint-disable-next-line no-unused-vars
   update: (x: any) => void;
   restoreOriginal: () => void;
   openGuidelines: () => void;
+  reviewData: UserReviewData;
+  saveTranscript: (
+    updatedContent: TranscriptContent,
+    onSuccessCallback?: () => void,
+    onNoEditsCallback?: () => void
+  ) => Promise<void>;
+  getUpdatedTranscript: () => TranscriptContent;
+  onOpen: () => void;
 }) => {
   const [isPreviewOnly, setIsPreviewOnly] = useState(false);
   const [isModalOpen, setIsModalopen] = useState(false);
-
+  const toast = useToast();
   const mdParser = new MarkdownIt();
+
+  const reviewSubmissionDisabled =
+    !!reviewData.branchUrl && !!reviewData.pr_url;
+
+  const { data: userSession } = useSession();
+  const isAdmin = userSession?.user?.permissions === "admin";
+  const { isLoading: saveLoading } = useUpdateTranscript();
+
+  const handleSave = async () => {
+    const onSuccessCallback = () => {
+      toast({
+        status: "success",
+        title: "Saved successfully",
+      });
+    };
+    const onNoEditsCallback = () => {
+      toast({
+        status: "warning",
+        title: "Unable to save because no edits have been made",
+      });
+    };
+    try {
+      await saveTranscript(
+        getUpdatedTranscript(),
+        onSuccessCallback,
+        onNoEditsCallback
+      );
+    } catch (err: any) {
+      toast({
+        status: "error",
+        title: "Error while saving",
+        description: err?.message,
+      });
+    }
+  };
 
   // Finish!
   function handleEditorChange({ text }: IHandleEditorChange) {
@@ -95,8 +160,13 @@ const EditTranscript = ({
         display="flex"
         flexDir="column"
       >
-        <Flex my={2} justifyContent={"space-between"}>
-          <Box>
+        <Flex
+          my={2}
+          flexWrap={"wrap"}
+          rowGap={4}
+          justifyContent={"space-between"}
+        >
+          <Flex gap={2}>
             <Button
               colorScheme="orange"
               onClick={openGuidelines}
@@ -108,9 +178,6 @@ const EditTranscript = ({
             >
               <FaBook /> Review Guidelines
             </Button>
-          </Box>
-
-          <Box>
             <Button
               colorScheme="gray"
               onClick={() => setIsModalopen(true)}
@@ -121,7 +188,43 @@ const EditTranscript = ({
             >
               Restore Original
             </Button>
-          </Box>
+          </Flex>
+
+          <Flex gap={2}>
+            <Button
+              size="sm"
+              colorScheme="orange"
+              variant="outline"
+              onClick={handleSave}
+              isLoading={saveLoading}
+            >
+              Save
+            </Button>
+            <Flex overflow="hidden" borderRadius="md" dir="row">
+              <Tooltip
+                label={
+                  reviewSubmissionDisabled
+                    ? "You cannot resubmit a submitted review, instead use save to update your submission"
+                    : undefined
+                }
+              >
+                <Button
+                  borderRadius="none"
+                  size="sm"
+                  colorScheme="orange"
+                  onClick={onOpen}
+                  isDisabled={reviewSubmissionDisabled}
+                >
+                  Submit {isAdmin ? `(${prRepo})` : ""}
+                </Button>
+              </Tooltip>
+              {isAdmin && (
+                <>
+                  <SubmitTranscriptMenu setPrRepo={setPrRepo} />
+                </>
+              )}
+            </Flex>
+          </Flex>
         </Flex>
         <Box h="full" id="simplemde-container-controller">
           <MdEditor
