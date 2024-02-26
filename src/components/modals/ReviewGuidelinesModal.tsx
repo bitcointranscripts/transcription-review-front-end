bf8b881c8da1f09835f454186410be73fe375096
kouloumos <kouloumosa@gmail.com>
2024-02-26 16:07:25 +0200

    support markdown for review guidelines
    use existing `markdown-it` package

--- src/components/modals/ReviewGuidelinesModal.tsx
+++ src/components/modals/ReviewGuidelinesModal.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Divider,
   Flex,
+  Heading,
   Link,
   ListItem,
   Modal,
@@ -14,8 +15,11 @@ import {
   Text,
   UnorderedList,
 } from "@chakra-ui/react";
+import MarkdownIt from "markdown-it";
 import { useRouter } from "next/router";
 import { forwardRef, useRef } from "react";
+
+const mdParser = new MarkdownIt();
 
 type Props = {
   isOpen: boolean;
@@ -65,7 +69,7 @@ const GuidelinesContent = forwardRef<HTMLButtonElement, GuidelinesContentProps>(
   ({ onCancel, isFirstTime }, ref) => {
     return (
       <>
-        <ModalHeader fontSize="2xl" fontWeight="bold">
+        <ModalHeader fontSize="2xl" fontWeight="bold" paddingBottom={0}>
           Review Guidelines
         </ModalHeader>
         <ModalBody>
@@ -83,11 +87,11 @@ const GuidelinesContent = forwardRef<HTMLButtonElement, GuidelinesContentProps>(
                 flexDir={"column"}
                 gap={2}
               >
-                <Text fontWeight="bold">{guideline.heading}</Text>
+                <Heading size='md'>{guideline.heading}</Heading>
                 <UnorderedList pl={3} color="gray.700" fontSize="14px">
                   {guideline.paragraphs.map((paragraph) => (
                     <ListItem key={paragraph}>
-                      <Text>{paragraph}</Text>
+                      <Text dangerouslySetInnerHTML={{ __html: mdParser.render(paragraph) }}></Text>
                     </ListItem>
                   ))}
                 </UnorderedList>
