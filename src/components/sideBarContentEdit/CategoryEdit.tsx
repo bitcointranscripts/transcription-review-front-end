749d404ded4f82d7e8e85a3901411f2577a3e5f4
balogunofafrica <balogunakanbi.k@gmail.com>
2024-03-04 08:13:42 +0100

    refactor: extend sideBarData for edit and submission

---
+++ src/components/sideBarContentEdit/CategoryEdit.tsx
@@ -0,0 +1,78 @@
+import { Box, Text } from "@chakra-ui/react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { OnlySelectField, SingleSelectField } from "./SelectField";
+import TextField from "./TextField";
+import styles from "./sidebarContentEdit.module.css";
+
+export function TextEdit({
+  editedData,
+  heading,
+  updateData,
+}: {
+  editedData: string;
+  heading: string;
+  updateData: (value: string) => void;
+}) {
+  return (
+    <Box>
+      <Text fontWeight={600} mb={2}>
+        {heading}
+      </Text>
+      <TextField editedData={editedData} updateData={updateData} />
+    </Box>
+  );
+}
+
+export function ListEdit(
+  props: (
+    | (React.ComponentProps<typeof OnlySelectField> & {
+        type?: "onlySelect";
+      })
+    | (React.ComponentProps<typeof SingleSelectField> & {
+        type?: "singleSelect";
+      })
+  ) & {
+    heading: string;
+  }
+) {
+  return (
+    <Box>
+      <Text fontWeight={600} mb={2}>
+        {props.heading}
+      </Text>
+      {props.type === "onlySelect" ? (
+        <OnlySelectField {...props} />
+      ) : (
+        <SingleSelectField {...props} />
+      )}
+    </Box>
+  );
+}
+
+export function DateEdit({
+  editedData,
+  heading,
+  updateData,
+}: {
+  editedData: Date | null;
+  heading: string;
+  updateData: (value: Date) => void;
+}) {
+  return (
+    <Box>
+      <Text display="inline-block" fontWeight={600} mb={2}>
+        {heading}
+      </Text>
+      <Text ml={3} display="inline-block" color="gray.400">
+        YYYY-MM-DD format
+      </Text>
+      <DatePicker
+        selected={editedData}
+        onChange={updateData}
+        dateFormat="yyyy-MM-dd"
+        className={styles.customDatePicker}
+      />
+    </Box>
+  );
+}
