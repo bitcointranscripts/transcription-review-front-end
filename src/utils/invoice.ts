8c4f3715ddaab08fbeed3a768358fecb321193c9
Theophilus <60826700+Extheoisah@users.noreply.github.com>
2024-03-22 14:20:25 +0100

    fix: validate invoice before making API pay request (#272)

---
+++ src/utils/invoice.ts
@@ -0,0 +1,44 @@
+import lightningPayReq from "bolt11";
+
+import { INVOICE_PREFIX } from "@/config/default";
+
+export const validateInvoice = (invoice: string) => {
+  if (!invoice) {
+    return {
+      success: false,
+      message: "Invoice is required",
+    };
+  }
+  try {
+    const decodedInvoice = lightningPayReq.decode(invoice);
+    if (!decodedInvoice) {
+      return {
+        success: false,
+        message: "Invalid invoice",
+      };
+    }
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") {
+      if (decodedInvoice.prefix !== INVOICE_PREFIX.mainnet) {
+        return {
+          success: false,
+          message: "Invalid invoice. Please use a mainnet invoice",
+        };
+      }
+    }
+    if (decodedInvoice.satoshis === null) {
+      return {
+        success: false,
+        message: "Invoice amount to send must be greater than 0",
+      };
+    }
+    return {
+      success: true,
+      message: "Invoice is valid",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "Invalid invoice",
+    };
+  }
+};
