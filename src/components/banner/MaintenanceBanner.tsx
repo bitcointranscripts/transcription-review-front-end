faffdcf56ee60055a35cd94ae3b138c8ac954bea
IgboPharaoh <solomoneze62@gmail.com>
2024-04-07 14:21:12 +0100

    create maintenance banner

---
+++ src/components/banner/MaintenanceBanner.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Box, Flex, Text } from "@chakra-ui/react";
+
+const MaintenanceBanner = () => {
+  return (
+    <Flex
+      bgColor="orange.100"
+      pos={"absolute"}
+      right={"0px"}
+      width={"100%"}
+      alignItems="center"
+      zIndex={99}
+    >
+      <Box
+        color={"gray.600"}
+        flex="1 1 100%"
+        fontWeight="medium"
+        textAlign="center"
+        padding={"16px"}
+      >
+        <Text
+          fontSize={["16px", "20px"]}
+          fontWeight={"black"}
+          textTransform={"uppercase"}
+          paddingBottom={"4apx"}
+        >
+          Service Disprution !!!
+        </Text>
+        <Text fontSize={["13px", "16px"]}>
+          Withdrawals are currently unavailable as we are experiencing a
+          downtime on our systems. <br /> We will remove this banner once
+          service is restored.
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
+
+export default MaintenanceBanner;
