1222d9f61cf88d66f5dbf0aa91ff4bc29bb07ff3
kouloumos <kouloumosa@gmail.com>
2024-05-24 16:08:11 +0300

    validate form onSubmit instead of onBlur
    - moved validation to submit
    - removed URL validation because it happens
    automatically when input type==url

--- src/components/modals/SuggestModal.tsx
+++ src/components/modals/SuggestModal.tsx
@@ -18,8 +18,7 @@ import {
   Text,
   useToast,
 } from "@chakra-ui/react";
-import { type FocusEventHandler, type FormEvent, useState } from "react";
-import { OnlySelectField } from "../sideBarContentEdit/SelectField";
+import { type FormEvent, useState, ChangeEvent } from "react";
 
 type SuggestModalProps = {
   handleClose: () => void;
@@ -49,26 +48,15 @@ export function SuggestModal({ handleClose, isOpen }: SuggestModalProps) {
     handleClose();
   };
 
-  const validateUrl: FocusEventHandler<HTMLInputElement> = (e) => {
+  const handleUrlChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormValues((v) => ({ ...v, url: e.target.value }))
+    setUrlError("");
+  }
+
+  const validateUrl = (urlString: string): boolean => {
     try {
-      const value = e.target.value.trim();
 
-      if (!value) {
-        setUrlError("");
-        return;
-      }
-
-      const url = new URL(value);
-      const hasCorrectProtocol = ["https:", "http:"].some((v) =>
-        v.includes(url.protocol)
-      );
-
-      if (!hasCorrectProtocol) {
-        setUrlError(
-          "Please include the correct protocol in the URL (http:// or https://)"
-        );
-        return;
-      }
+      const url = new URL(urlString.trim());
 
       const urlExists = selectableListData?.media.some((mediaUrl) =>
         compareUrls(new URL(mediaUrl), url)
@@ -79,18 +67,21 @@ export function SuggestModal({ handleClose, isOpen }: SuggestModalProps) {
         // This would help users who want to add a source by pointing them to the existing transcript.
         // Modifications to <https://btctranscripts.com/status.json> are needed to make the transcript URL accessible.
         setUrlError("A transcript for this source already exists");
-        return;
+        return false;
       }
 
-      setUrlError("");
-      setFormValues((v) => ({ ...v, url: value }));
+      return true;
     } catch {
       setUrlError("Invalid URL, please enter a valid URL");
+      return false;
     }
   };
 
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
+
+    const isUrlValid = validateUrl(formValues.url);
+    if (!isUrlValid) return;
 
     createPR.mutate(
       {
@@ -117,15 +108,14 @@ export function SuggestModal({ handleClose, isOpen }: SuggestModalProps) {
           toast({
             status: "success",
             title: "Suggestion submitted successfully",
-            onCloseComplete: resetAndCloseForm,
           });
+          resetAndCloseForm()
         },
       }
     );
   };
 
-  const formIsComplete =
-    !!(formValues.title.trim() && formValues.url.trim()) && !urlError;
+  const formIsComplete = !!(formValues.title.trim() && formValues.url.trim());
 
   return (
     <Modal isOpen={isOpen} onClose={resetAndCloseForm} isCentered>
@@ -137,86 +127,84 @@ export function SuggestModal({ handleClose, isOpen }: SuggestModalProps) {
         overflowY={"auto"}
         borderRadius={20}
       >
-          <ModalHeader>
-            <Text
-              fontSize={{ base: "md", lg: "xl" }}
-              mb={{ base: "4px", lg: "8px" }}
-              textAlign="center"
-            >
-              Suggest a Source for Transcription
-            </Text>
-            <Text
-              fontWeight="600"
-              fontSize={{ base: "xs", lg: "sm" }}
-              textAlign="center"
-            >
-              We manually review every suggestion to ensure it meets our
-              standards for reliable,
-              technical Bitcoin content.
-            </Text>
-          </ModalHeader>
-          <form onSubmit={handleSubmit}>
-            <ModalBody>
-              <Flex flexDir="column" gap={{ base: "24px", lg: "32px" }}>
-                <FormControl isRequired gap={{ base: "6px", lg: "xs" }}>
-                  <FormLabel>Title</FormLabel>
-                  <Input
-                    placeholder="Add transcript title"
-                    onChange={(e) =>
-                      setFormValues((v) => ({ ...v, title: e.target.value }))
-                    }
-                    required
-                  />
-                </FormControl>
-                <FormControl isRequired gap={{ base: "6px", lg: "xs" }}>
-                  <FormLabel>Source&apos;s URL</FormLabel>
-                  <Input
-                    type="url"
-                    placeholder="https://"
-                    onBlur={validateUrl}
-                    required
-                    maxLength={255}
-                  />
-                  {(!!urlError || !formValues.url) && (
-                    <FormHelperText
-                      fontSize={{ base: "xs", lg: "sm" }}
-                      color={urlError ? "red" : undefined}
-                      fontWeight="medium"
-                    >
-                      {urlError
-                        ? urlError
-                        : "Please enter the full URL, including http:// or https://"}
-                    </FormHelperText>
-                  )}
-                </FormControl>
-              </Flex>
-            </ModalBody>
-            <ModalFooter             
-              gap={{ base: "8px", lg: "md" }}
+        <ModalHeader>
+          <Text
+            fontSize={{ base: "md", lg: "xl" }}
+            mb={{ base: "4px", lg: "8px" }}
+            textAlign="center"
+          >
+            Suggest a Source for Transcription
+          </Text>
+          <Text
+            fontWeight="600"
+            fontSize={{ base: "xs", lg: "sm" }}
+            textAlign="center"
+          >
+            We manually review every suggestion to ensure it meets our
+            standards for reliable,
+            technical Bitcoin content.
+          </Text>
+        </ModalHeader>
+        <form onSubmit={handleSubmit}>
+          <ModalBody>
+            <Flex flexDir="column" gap={{ base: "24px", lg: "32px" }}>
+              <FormControl isRequired gap={{ base: "6px", lg: "xs" }}>
+                <FormLabel>Title</FormLabel>
+                <Input
+                  placeholder="Add transcript title"
+                  onChange={(e) =>
+                    setFormValues((v) => ({ ...v, title: e.target.value }))
+                  }
+                  required
+                />
+              </FormControl>
+              <FormControl isRequired gap={{ base: "6px", lg: "xs" }} isInvalid={!!urlError}>
+                <FormLabel>Source&apos;s URL</FormLabel>
+                <Input
+                  type="url"
+                  placeholder="https://"
+                  onChange={handleUrlChange}
+                  required
+                  maxLength={255}
+                />
+                <FormHelperText
+                  fontSize={{ base: "xs", lg: "sm" }}
+                  color={urlError ? "red" : undefined}
+                  fontWeight="medium"
+                >
+                  {urlError
+                    ? urlError
+                    : "Please enter the full URL, including http:// or https://"}
+                </FormHelperText>
+              </FormControl>
+            </Flex>
+          </ModalBody>
+          <ModalFooter
+            gap={{ base: "8px", lg: "md" }}
+            w="full"
+          >
+            <Button
               w="full"
+              mx="auto"
+              rounded="10px"
+              isDisabled={createPR.isLoading}
+              onClick={handleClose}
             >
-              <Button
-                w="full"
-                mx="auto"
-                rounded="10px"
-                isDisabled={createPR.isLoading}
-                onClick={handleClose}
-              >
-                Cancel
-              </Button>
-              <Button
-                w="full"
-                isLoading={createPR.isLoading}
-                mx="auto"
-                colorScheme="orange"
-                rounded="10px"
-                isDisabled={!formIsComplete}
-                type="submit"
-              >
-                Suggest source
-              </Button>
-            </ModalFooter>
-          </form>
+              Cancel
+            </Button>
+            <Button
+              w="full"
+              isLoading={createPR.isLoading}
+              mx="auto"
+              colorScheme="orange"
+              rounded="10px"
+              isDisabled={!formIsComplete}
+              type="submit"
+            >
+              Suggest source
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   );
